5bfba189503b60672fd329b57d3d96e620fa39a9
AllTheLeaves <64288764+AllTheLeaves@users.noreply.github.com>
2020-04-28 18:05:29 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -1,20 +1,31 @@
-function chooseTheme(theme) {
-  if (theme == "dark") {
-    document.body.classList.remove("lightTheme");
-    document.body.classList.add("darkTheme");
-    for (var i1 = 0; i1 < document.getElementsByClassName("btnStyle").length; i1++) {
-      document.getElementsByClassName("btnStyle")[i1].classList.remove("lightTheme");
-      document.getElementsByClassName("btnStyle")[i1].classList.add("darkTheme");
+barVal = {
+  HatchEggs: 0;
+  TendEggs: 0;
+}
+
+themeSelection = ["dark","light","bad"]
+loopItemStylesI = ["logoTxt","topBar"]
+loopItemStylesC = ["btnStyle","txtStyle","sectionStyle","btnBar"]
+
+function chooseTheme(chosenTheme) {
+  for (var i1 = 0; i1 < loopItemStylesC.length; i1++) {
+    for (var i2 = 0; i2 < document.getElementsByClassName(loopItemStylesC[i1]).length; i2++) {
+      for (var i3 = 0; i3 < themeSelection.length; i3++) {
+        document.body.classList.remove(themeSelection[i3] + "Theme");
+        document.getElementsByClassName(loopItemStylesC[i1])[i2].classList.remove(themeSelection[i3] + "Theme");
+      }
+      document.body.classList.add(chosenTheme + "Theme");
+      document.getElementsByClassName(loopItemStylesC[i1])[i2].classList.add(chosenTheme + "Theme");
     }
-    console.log("zoop");
   }
-  if (theme == "light") {
-    document.body.classList.remove("darkTheme");
-    document.body.classList.add("lightTheme");
-    for (var i1 = 0; i1 < document.getElementsByClassName("btnStyle").length; i1++) {
-      document.getElementsByClassName("btnStyle")[i1].classList.remove("darkTheme");
-      document.getElementsByClassName("btnStyle")[i1].classList.add("lightTheme");
+  for (var i1 = 0; i1 < loopItemStylesI.length; i1++) {
+    for (var i2 = 0; i2 < themeSelection.length; i2++) {
+      document.getElementById(loopItemStylesI[i1]).classList.remove(themeSelection[i2] + "Theme");
     }
-    console.log("boop");
+    document.getElementById(loopItemStylesI[i1]).classList.add(chosenTheme + "Theme");
   }
+}
+
+function btnPressed(btnClicked) {
+    document.getElementById("btnBar" + btnClicked).style.width = barVal.btnClicked;
 }
